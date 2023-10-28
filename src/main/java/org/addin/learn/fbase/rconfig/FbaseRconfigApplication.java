c6d66f1c235904e9b3b212c370bc73045ed5c50e
Addinul <addins3009@gmail.com>
2023-10-28 16:35:18 +0700

    init

---
+++ src/main/java/org/addin/learn/fbase/rconfig/FbaseRconfigApplication.java
@@ -0,0 +1,13 @@
+package org.addin.learn.fbase.rconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbaseRconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbaseRconfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/addin/learn/fbase/rconfig/FbaseRconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.addin.learn.fbase.rconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbaseRconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
